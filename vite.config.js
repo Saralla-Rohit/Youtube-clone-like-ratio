812913a8cf26ecf865203103d2578e7f2695fa0a
rohit sarla <rohitsarla69@gmail.com>
2025-01-17 13:48:13 +0530

    fixed ports issue

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ensure the server binds to all interfaces (public IPs)
+    port: process.env.PORT || 5173, // Use the PORT environment variable (Render will set this automatically)
+  },
 })
